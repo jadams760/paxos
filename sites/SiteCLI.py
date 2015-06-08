f9d3b9389dc98441290c5bb8fddbcb2ee7ed3b7f
Max Stritzinger <mstritzinger@live.com>
2015-06-08 15:04:39 -0700

    adding starts

--- sites/SiteCLI.py
+++ sites/SiteCLI.py
@@ -3,78 +3,32 @@ import pickle
 import re
 import os
 import socket
+import network
 import time
 import random
-import ClientServer
 import sys
 
 
-sites = [('localhost',9001),('localhost',9002),('localhost',9003),('localhost',9004),('localhost',9005)]
-#sites = [('localhost',9001)]
-
-
-class ClientCLI(threading.Thread):
-    def __init__(self, hostname, port, sites):
-        self.thread = threading.Thread.__init__(self)
+class SiteCLI(threading.Thread):
+    def __init__(self, hostname, port, sites, siteID):
+        threading.Thread.__init__(self)
+        self.failLock = threading.Event()
+        self.locks = { 'printLock': threading.RLock(), 'failLock': self.failLock }
         self.hostname = hostname
         self.port = port
         self.sites = sites
-        self.leader =0
-        self.server = ClientServer.ClientServer(self.hostname,self.port)
-        
-        
-    def myConnect(self,sock,host,port):
-        sock.connect((host,port))
-
-    def mySend(self,sock,send):
-        sock.send(pickle.dumps(send))
-
-
+        self.network = network.Network(hostname, port, sites, siteID, self.locks)
 
     def run(self):
-        self.server.start()
+        self.network.start()
         while True:
 
-            userInput = input("Please Enter One of the Following and Press Enter:\n(1) Read\n(2) Post\n(3) Exit\n")
+            userInput = input("Please Enter One of the Following and Press Enter:\n(1) Fail\n(2) Recover\n")
             if userInput == "1":
-                leaderFound = False
-                while leaderFound == False:
-                    print("Connecting to Site " + str(self.leader))
-                    try:
-                        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        self.myConnect(sock, self.sites[self.leader][0], self.sites[self.leader][1])
-                        leaderFound = True
-                    except ConnectionRefusedError:
-                        print('connection refused')
+                self.failLock.clear()
 
-                    self.leader = random.randint(0,4)
-  
-                send = {'action':'read', 'return':(self.hostname, self.port), 'message':'', 'source':'client'}
-                self.mySend(sock,send)
-                sock.close()
-            
-                
             elif userInput == "2":
-                userInput = input("Please enter a message to append:\n")
-                userInput = userInput[:140]
-                leaderFound = False
-                while leaderFound == False:
-                    print("Connecting to Site " + str(self.leader))
-                    try:
-                        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        self.myConnect(sock, self.sites[self.leader][0], self.sites[self.leader][1])
-                        leaderFound = True
-                    except ConnectionRefusedError:
-                        print('connection refused')
-
-                    self.leader = random.randint(0,4)
-
-                send = {'action':'post', 'return':(self.hostname, self.port), 'message':userInput, 'source':'client'}
-                self.mySend(sock,send)
-                sock.close()
-                    
-            elif userInput == "3":
-                return False
+                self.failLock.set()
 
             else:
                 print("Incorrect input type\n")
---
+++ sites/site5Start.py
@@ -0,0 +1,31 @@
+from network import *
+from pprint import pprint
+import threading
+import SiteCLI
+import time
+
+printLock = threading.RLock()
+class Dummy(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serverAddress = ('localhost', 9999)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(serverAddress)
+        sock.listen(1)
+        while 1:
+            conn, addr = sock.accept()
+            message = pickle.loads(conn.recv(4092))
+            conn.send('ack')
+            with printLock:
+                pprint(message)
+            conn.close()
+
+
+
+if __name__ == '__main__':
+
+    sites = { 5:('localhost', 10000), 1:('localhost', 10001), 2:('localhost', 10002), 3:('localhost', 10003), 4:('localhost', 10004) }
+    site = SiteCLI( 'localhost', 10000, sites, 5)
+    site.start()
